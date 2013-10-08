004b52978dc86c974739fbe4466a3437d2604f54
Thomas Gull <tgull1@dmail.dixie.edu>
2013-10-08 09:18:22 -0600

    GameState updated to use Player and World objects

--- src/Spellbyte/states/GameState.cpp
+++ src/Spellbyte/states/GameState.cpp
@@ -5,28 +5,36 @@ namespace SpellByte
 {
     GameState::GameState()
     {
+        mCollisionTools = 0;
         Quit = false;
+    }
+
+    GameState::~GameState()
+    {
+        SceneMgr = 0;
     }
 
     void GameState::enter()
     {
         LOG("GameState: enter");
 
-        SceneMgr = APP->OgreRoot->createSceneManager(Ogre::ST_GENERIC, "GameSceneMgr");
-        SceneMgr->setAmbientLight(Ogre::ColourValue(0.7f, 0.7f, 0.7f));
-
+        SceneMgr = APP->SceneMgr;
         Camera = SceneMgr->createCamera("GameCamera");
-        Camera->setPosition(Ogre::Vector3(-1800, -300, -1100));
-        Camera->lookAt(Ogre::Vector3(-300, 300, 0));
-        Camera->setNearClipDistance(1);
-
         Camera->setAspectRatio(Ogre::Real(APP->Viewport->getActualWidth())
-                                    / Ogre::Real(APP->Viewport->getActualHeight()));
+                                / Ogre::Real(APP->Viewport->getActualHeight()));
 
         APP->Viewport->setCamera(Camera);
+        APP->ceguiContext->getMouseCursor().hide();
 
+        player.init(SceneMgr, Camera);
+
+        //CameraMan = new OgreBites::SdkCameraMan(Camera);
+
+        gameWorld.loadWorld();
         buildGUI();
         createScene();
+        mCollisionTools = new MOC::CollisionTools(SceneMgr, gameWorld.terrainGroup);
+        player.setCollisionHanlder(mCollisionTools);
     }
 
     bool GameState::pause()
@@ -44,6 +52,8 @@ namespace SpellByte
 
         APP->Viewport->setCamera(Camera);
 
+        APP->ceguiContext->getMouseCursor().hide();
+
         Quit = false;
     }
 
@@ -51,20 +61,40 @@ namespace SpellByte
     {
         APP->Log->logMessage("GameState: exit");
 
+        destroyScene();
+
+        if(mCollisionTools)
+        {
+            delete mCollisionTools;
+        }
+
         SceneMgr->destroyCamera(Camera);
 
-        if(SceneMgr)
-            APP->OgreRoot->destroySceneManager(SceneMgr);
+        //SceneMgr->destroyQuery(RSQ);
+        SceneMgr = 0;
     }
 
     void GameState::createScene()
     {
+        gameWorld.createScene();
 
+        CEGUI::WindowManager &wmgr = CEGUI::WindowManager::getSingleton();
+        CEGUI::Window *wRoot = wmgr.createWindow("DefaultWindow", "root");
+        APP->ceguiContext->setRootWindow( wRoot );
+        debugBox = wmgr.createWindow("TaharezLook/StaticText", "textbox");
+        wRoot->addChild(debugBox);
+    }
+
+    void GameState::destroyScene()
+    {
+        gameWorld.destroyScene();
     }
 
     void GameState::update(const Ogre::FrameEvent &evt)
     {
+        player.update(evt);
 
+        debugBox->setText(player.getDebugString().c_str());
     }
 
     void GameState::handleEvents()
@@ -72,9 +102,28 @@ namespace SpellByte
         while(CONTROL->hasEvent())
         {
             const UserEvent *tmp = CONTROL->getEvent();
-            if(tmp->getType() == UserEvent::ESCAPE)
+            if(tmp->getType() > UserEvent::PLAYER_ACTION_START && tmp->getType() < UserEvent::PLAYER_ACTION_END)
             {
+                player.handleEvent(tmp->getType());
+                continue;
+            }
+            else if(tmp->getType() > UserEvent::WORLD_ACTION_START && tmp->getType() < UserEvent::WORLD_ACTION_END)
+            {
+                gameWorld.handleEvent(tmp->getType());
+                continue;
+            }
+            switch(tmp->getType())
+            {
+            case UserEvent::ESCAPE:
                 pushState(findByName("PauseState"));
+                break;
+            case UserEvent::MOUSE_MOVE:
+                int x, y;
+                tmp->getRel(x, y);
+                player.setRotation(x, y);
+                break;
+            default:
+                break;
             }
         }
     }
--- src/Spellbyte/states/GameState.h
+++ src/Spellbyte/states/GameState.h
@@ -4,39 +4,45 @@
 #include "../BaseState.h"
 
 #include <OgreSubEntity.h>
-#include <OgreMaterialManager.h>
+#include "../Player.h"
+#include "../CollisionTools.h"
+#include "../World.h"
 
 namespace SpellByte
 {
-    enum QueryFlags
-    {
-        OGRE_HEAD_MASK      = 1<<0,
-        CUBE_MASK           = 1<<1
-    };
-
     class GameState : public BaseState
     {
     public:
         GameState();
+        ~GameState();
 
         DECLARE_BASESTATE_CLASS(GameState)
 
         void enter();
         void createScene();
+        void destroyScene();
         void exit();
         bool pause();
         void resume();
 
-        void moveCamera();
         void buildGUI(){};
 
         void update(const Ogre::FrameEvent &evt);
         void handleEvents();
 
+        MOC::CollisionTools* mCollisionTools;
+
     private:
-        bool                    LMouseDown;
-        bool                    RMouseDown;
-        bool                    SettingsMode;
+        bool useArtifex;
+
+        // GUI
+        CEGUI::Window *debugBox;
+
+        // Player
+        Player player;
+
+        // World
+        World gameWorld;
     };
 }
 
